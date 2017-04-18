1edaa8927a2ed1942dc2274ef5a91dda47870c8e
long261vn <long261vn@yahoo.com>
2017-04-17 23:17:08 -0700

    upload correction

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var http = require('http');
 var fs = require('fs');
 var extract = require('./extract');
 var wss = require('./websockets-server');
+var mime = require('mime');
 
 var handleError = function (err, res) {
     res.writeHead(404);
@@ -14,11 +15,14 @@ var server = http.createServer(function (req, res) {
     console.log('Responding to a request.');
 
     var filePath = extract(req.url);
+    var fileType = mime.lookup(filePath);
     fs.readFile(filePath, function (err, data) {
         if (err) {
             handleError(err, res);
             return;
         } else {
+            console.log(fileType);
+            res.setHeader('Content-Type', fileType);
             res.end(data);
         }
     });
